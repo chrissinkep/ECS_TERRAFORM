a26d759f1d315cde5f09a4b07218c7baf61c1f2f
chrissinkep <chrissinkep@gmail>
2025-10-19 15:55:42 -0700

    commit

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 2.63"
+  version = "~> 5.0"
 }
 
 
 terraform {
   backend "s3" {
-    bucket = "ecsworkshopbucket"
+    bucket = "chrissinkep-terraform-remote-state"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
