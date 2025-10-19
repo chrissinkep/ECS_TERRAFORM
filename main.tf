a959b07649deea8d1b5ebf10711fc3b40892b04c
chrissinkep <chrissinkep@gmail>
2025-10-19 08:04:50 -0700

    commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "chrissinkep-terraform-remote-state"
+    bucket = "ecsworkshopbucket"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
