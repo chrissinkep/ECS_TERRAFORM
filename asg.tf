75d28dd1dc29ed681500d550183e6b97749285e2
chrissinkep <chrissinkep@gmail>
2025-10-19 18:09:47 -0700

    commit

--- asg.tf
+++ asg.tf
@@ -21,7 +21,7 @@ data "aws_ami" "amazon_linux" {
 resource "aws_security_group" "ec2-sg" {
   name        = "allow-all-ec2"
   description = "allow all"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = data.aws_vpc.main.id
   ingress {
     from_port   = 0
     to_port     = 0
@@ -40,35 +40,27 @@ resource "aws_security_group" "ec2-sg" {
   }
 }
 
-resource "aws_launch_template" "lt" {
-  name_prefix   = "ecs-launch-template-"
+resource "aws_launch_configuration" "lc" {
+  name          = "test_ecs"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  key_name      = var.key_name
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ecs_service_role.name
-  }
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = [aws_security_group.ec2-sg.id]
-  }
-
-  user_data = base64encode(<<EOF
-#!/bin/bash
-sudo apt-get update
-echo "ECS_CLUSTER=${var.cluster_name}" >> /etc/ecs/ecs.config
-EOF
-  )
-
   lifecycle {
     create_before_destroy = true
   }
+  iam_instance_profile        = aws_iam_instance_profile.ecs_service_role.name
+  key_name                    = var.key_name
+  security_groups             = [aws_security_group.ec2-sg.id]
+  associate_public_ip_address = true
+  user_data                   = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo echo "ECS_CLUSTER=${var.cluster_name}" >> /etc/ecs/ecs.config
+EOF
 }
 
 resource "aws_autoscaling_group" "asg" {
   name                      = "test-asg"
+  launch_configuration      = aws_launch_configuration.lc.name
   min_size                  = 3
   max_size                  = 4
   desired_capacity          = 3
@@ -76,14 +68,8 @@ resource "aws_autoscaling_group" "asg" {
   health_check_grace_period = 300
   vpc_zone_identifier       = module.vpc.public_subnets
 
-  launch_template {
-    id      = aws_launch_template.lt.id
-    version = "$Latest"
-  }
-
   target_group_arns     = [aws_lb_target_group.lb_target_group.arn]
   protect_from_scale_in = true
-
   lifecycle {
     create_before_destroy = true
   }
