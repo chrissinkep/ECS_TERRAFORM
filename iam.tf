7ff60fa021d577d19f4d1405695e63977da8a65b
chrissinkep <chrissinkep@gmail>
2025-10-20 20:43:46 -0700

    commit

--- iam.tf
+++ iam.tf
@@ -1,15 +1,15 @@
-resource "aws_iam_role" "ecs-instance-role" {
+resource "aws_iam_role" "ecs_instance_role" {
   name = "ecs-instance-role-test-web"
   path = "/"
 
   assume_role_policy = <<EOF
 {
-  "Version": "2008-10-17",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": ["ec2.amazonaws.com"]
+        "Service": "ec2.amazonaws.com"
       },
       "Effect": "Allow"
     }
@@ -18,11 +18,19 @@ resource "aws_iam_role" "ecs-instance-role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
-  role       = aws_iam_role.ecs-instance-role.name
+# ECS Agent permissions
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attachment" {
+  role       = aws_iam_role.ecs_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_instance_profile" "ecs_service_role" {
-  role = aws_iam_role.ecs-instance-role.name
+# ECR Read-Only so ECS can pull images
+resource "aws_iam_role_policy_attachment" "ecs_ecr_readonly" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile-test-web"
+  role = aws_iam_role.ecs_instance_role.name
 }
