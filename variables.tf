328c63d651fbedf3282ddbd62292b035b36f8915
chrissinkep <chrissinkep@gmail>
2025-10-20 19:28:02 -0700

    commit

--- variables.tf
+++ variables.tf
@@ -1,19 +1,34 @@
-variable "cluster_name" {
-  description = "Name of the ECS cluster"
+# AWS region
+variable "region" {
+  description = "AWS region"
   type        = string
-  default     = "ecs-web-cluster"
+  default     = "us-east-1"
 }
 
+# Name of the ECS cluster
+variable "cluster_name" {
+  description = "ECS Cluster name"
+  type        = string
+  default     = "web-cluster"
+}
+
+# EC2 Key Pair name
 variable "key_name" {
-  description = "EC2 Key Pair name for SSH access"
+  description = "EC2 Key Pair name"
   type        = string
   default     = "ecs_workshop"
 }
 
+# Public subnet IDs for ECS instances and ALB
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+  default     = ["subnet-11111111","subnet-22222222","subnet-33333333"]
+}
+
+# ECR image URI
 variable "ecr_image_uri" {
   description = "ECR image URI for the container"
   type        = string
-  default     = "863218674815.dkr.ecr.us-east-1.amazonaws.com/workshop"
+  default     = "863218674815.dkr.ecr.us-east-1.amazonaws.com/workshop:latest"
 }
-
-# No need for public_subnets input — we will get it from the VPC module
